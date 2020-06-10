592fb44bc976fc6b48925c877a52c94f702b81bd
FinneganHunter <49160410+FinneganHunter@users.noreply.github.com>
2020-06-10 12:25:49 -0400

    Add files via upload

---
+++ sweet-pumpkins/src/header/Header.js
@@ -0,0 +1,17 @@
+// Header.js
+// https://codetheweb.blog/2017/10/22/wrapping-and-grouping-elements/
+
+import React from "react";
+import "./Header.css";
+import '../index.css'
+
+const Header = () => (
+    <div className="Header">        
+
+      <a href="https://sweetpumpkins.codecamps.com/">Sweet Pumpkins</a>
+  
+    </div>
+  )
+
+export default Header;
+
