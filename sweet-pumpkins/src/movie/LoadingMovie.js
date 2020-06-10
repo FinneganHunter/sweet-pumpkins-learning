2a63bbeb09f90e430fc71538e183fe0cba266a8e
FinneganHunter <49160410+FinneganHunter@users.noreply.github.com>
2020-06-10 12:26:57 -0400

    Create LoadingMovie.js

---
+++ sweet-pumpkins/src/movie/LoadingMovie.js
@@ -0,0 +1,9 @@
+// LoadingMovie.js
+
+import React from "react";
+
+// const Zoomin = styled.div`animation: 2s 2s 2s`;
+
+const LoadingMovie = () => <h2>Loading Movie</h2>
+
+export default LoadingMovie;
